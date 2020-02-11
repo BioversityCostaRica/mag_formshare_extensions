077cfa61a99ab477cbcdd3f3c8a67e1e6be675df
Carlos Quiros <cquiros@qlands.com>
2020-02-11 15:07:59 -0600

    Frontend plugin

--- mag_frontend/mag_frontend/plugin.py
+++ mag_frontend/mag_frontend/plugin.py
@@ -1,38 +1,17 @@
 import formshare.plugins as plugins
 import formshare.plugins.utilities as u
-from .views import MyPublicView, MyPrivateView
 import sys
 import os
 
 
 class mag_frontend(plugins.SingletonPlugin):
-    plugins.implements(plugins.IRoutes)
     plugins.implements(plugins.IConfig)
     plugins.implements(plugins.ITranslation)
-
-    def before_mapping(self, config):
-        # We don't add any routes before the host application
-        return []
-
-    def after_mapping(self, config):
-        # We add here a new route /json that returns a JSON
-        custom_map = [
-            u.add_route(
-                "plugin_mypublicview", "/mypublicview", MyPublicView, "public.jinja2"
-            ),
-            u.add_route(
-                "plugin_myprivateview",
-                "/user/{userid}/myprivateview",
-                MyPrivateView,
-                "private.jinja2",
-            ),
-        ]
-
-        return custom_map
 
     def update_config(self, config):
         # We add here the templates of the plugin to the config
         u.add_templates_directory(config, "templates")
+        u.add_static_view(config, "mag_branding", "static")
 
     def get_translation_directory(self):
         module = sys.modules["mag_frontend"]
